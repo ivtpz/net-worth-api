152430ea978694edfa1009632517cd6c3abe766d
Ivey Topaz <iveytopaz@gmail.com>
2019-05-07 15:26:32 -0600

    Add additional tests

--- UnitTests/ExchangeRateControllerTests.cs
+++ UnitTests/ExchangeRateControllerTests.cs
@@ -1,10 +1,74 @@
-﻿using System;
-namespace networthunittests
+﻿using NUnit.Framework;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using exchange_rates.Controllers;
+using exchange_rates.Models; 
+using Moq;
+
+namespace ExchangeRateTests
 {
-    public class ExchangeRateControllerTests
+    [TestFixture]
+    public class WhenGettingExchangeRate
     {
-        public ExchangeRateControllerTests()
+
+        private readonly ConvertController _convertController;
+
+        public WhenGettingExchangeRate()
         {
+            var configMock = new Mock<IConfiguration>();
+            configMock.SetupGet(x => x[It.IsAny<string>()]).Returns("../../../../exchange-rates/data.json");
+            _convertController = new ConvertController(configMock.Object);
         }
+
+        [Test]
+        public void Should_Get_Conversion_Rate_Between_Currencies()
+        {
+            var result = _convertController.Get(1, 3).Result as OkObjectResult;
+            var expected = new ExchangeRate
+            {
+                Name = "GBP",
+                Symbol = "£",
+                ID = 3,
+                Rate = 0.76m
+            };
+            var exchangeRate = result.Value as ExchangeRate;
+            Assert.AreEqual(expected.Name, exchangeRate.Name);
+            Assert.AreEqual(expected.Symbol, exchangeRate.Symbol);
+            Assert.AreEqual(expected.ID, exchangeRate.ID);
+            Assert.AreEqual(expected.Rate, exchangeRate.Rate);
+
+        }
+
+        [Test]
+        public void Should_Error_If_Currency_Id_Does_Not_Exist()
+        {
+            var result = _convertController.Get(1, 13);
+            var httpStatus = result.Result as StatusCodeResult;
+            Assert.AreEqual(400, httpStatus.StatusCode);
+        }
+    }
+
+    [TestFixture]
+    public class WhenGettingExchangeRates
+    {
+
+        private readonly CurrenciesController _currencyController;
+
+        public WhenGettingExchangeRates()
+        {
+            var configMock = new Mock<IConfiguration>();
+            configMock.SetupGet(x => x[It.IsAny<string>()]).Returns("../../../../exchange-rates/data.json");
+            _currencyController = new CurrenciesController(configMock.Object);
+        }
+
+        [Test]
+        public void Should_Get_All_Currencies()
+        {
+            var result = _currencyController.Get().Result as OkObjectResult;
+            var currencies = result.Value as Currency[];
+            Assert.IsInstanceOf<Currency>(currencies[0]);
+            Assert.AreEqual(10, currencies.Length);
+        }
+
     }
 }
--- UnitTests/ExchangeRateTests.cs
+++ UnitTests/ExchangeRateTests.cs
@@ -1,26 +1,34 @@
 ﻿using NUnit.Framework;
-using exchangerates.Services;
-using exchangerates.Models;
+using Microsoft.Extensions.Configuration;
+using exchange_rates.Services;
+using exchange_rates.Models;
 using Moq;
-using System.IO;
 
 namespace ExchangeRateTests
 {
     [TestFixture]
-    public class WhenGettingExchangeRate
+    public class WhenReadingExchangeRate
     {
 
         private readonly ExchangeRateData _exchangeRateData;
 
-        public WhenGettingExchangeRate()
+        public WhenReadingExchangeRate()
         {
-            _exchangeRateData = new ExchangeRateData();
+            var configMock = new Mock<IConfiguration>();
+            configMock.SetupGet(x => x[It.IsAny<string>()]).Returns("../../../../exchange-rates/data.json");
+            _exchangeRateData = new ExchangeRateData(configMock.Object);
         }
 
         [Test]
         public void Should_Get_ExchangeRates()
         {
             Assert.IsInstanceOf<ExchangeRate>(_exchangeRateData.ExchangeRates[0]);
+        }
+
+        [Test]
+        public void Should_Get_Currencies()
+        {
+            Assert.IsInstanceOf<Currency>(_exchangeRateData.ExchangeRates[0]);
         }
 
     }
